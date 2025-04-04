abb3edcf15d76495b6f441866622723b92672552
pratchayapol <pratchayapol2543@gmail.com>
2025-04-05 00:32:45 +0700

    aa

--- src/student/dashboard.php
+++ src/student/dashboard.php
@@ -144,8 +144,6 @@ $course_level = $_SESSION['course_level'] ?? '';
             position: fixed;
             top: 0;
             left: 0;
-            width: 100%;
-            height: 100%;
             z-index: -1; /* ให้ภาพพื้นหลังอยู่หลังเนื้อหา */
         }
 
