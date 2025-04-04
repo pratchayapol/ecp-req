49eae96c4479357287f23e6c6fe6f5a21789e9ab
pratchayapol <pratchayapol2543@gmail.com>
2025-04-04 23:51:04 +0700

    aa

--- src/student/dashboard.php
+++ src/student/dashboard.php
@@ -66,8 +66,8 @@ $course_level = $_SESSION['course_level'] ?? '';
 
         <!-- Main Content -->
         <div class="flex-1 flex flex-col justify-between bg-white/60 mt-6 mb-6 sm:mt-6 sm:me-6 rounded-[20px]">
-            <div class="p-8">
-                <div class="bg-white rounded-lg shadow-lg h-96">
+            <div class="p-8 flex justify-center">
+                <div class="bg-white rounded-lg shadow-lg h-96 flex flex-col items-center justify-center p-4 w-full sm:w-3/4">
                     <h1 class="text-orange-500 bg-white p-2 text-xl h-12 font-bold shadow-md rounded-[12px] text-center">ประชาสัมพันธ์</h1>
                 </div>
             </div>
