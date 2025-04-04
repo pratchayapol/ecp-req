99da43dde21296083d9f437b6eefab18a7760e00
pratchayapol <pratchayapol2543@gmail.com>
2025-04-04 17:24:03 +0700

    xc

--- src/student/dashboard.php
+++ src/student/dashboard.php
@@ -1,12 +1,30 @@
+<?php
+session_start();
+
+?>
 <!DOCTYPE html>
 <html lang="th">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Dashboard</title>
     <script src="https://cdn.tailwindcss.com"></script>
+
+    <!-- Custom fonts for this template-->
+    <link rel="shortcut icon" href="./image/favicon.ico" type="image/x-icon">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+Thai:wght@300&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
+    <link rel="stylesheet" href="./css/fonts.css">
+    <link rel="stylesheet" href="./css/bg.css">
+    <!-- animation -->
+    <link rel="stylesheet" href="./css/animation.css">
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
 </head>
-<body class="bg-cover bg-center bg-no-repeat" style="background-image: url('background.jpg');">
+
+<body class="bg-cover bg-center bg-no-repeat bg">
 
 <div class="flex h-screen">
     <!-- Sidebar -->
