cc93a23856db343cd798b99950e3cf26faf2b69e
pratchayapol <pratchayapol2543@gmail.com>
2025-04-05 13:03:49 +0700

    a

--- src/student/form_re06.php
+++ src/student/form_re06.php
@@ -25,7 +25,7 @@ $course_level = $_SESSION['course_level'] ?? '';
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Dashboard</title>
+    <title>แบบฟอร์ม RE.06</title>
     <script src="https://cdn.tailwindcss.com"></script>
 
     <!-- FontAwesome (สำหรับไอคอน) -->
