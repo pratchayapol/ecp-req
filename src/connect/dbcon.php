919cbc81d09346206f912dce20c903e70199d4aa
pratchayapol <pratchayapol2543@gmail.com>
2025-04-05 12:02:10 +0700

    aa

--- src/connect/dbcon.php
+++ src/connect/dbcon.php
@@ -12,7 +12,7 @@ try {
     // ตั้งค่า PDO ให้แสดงข้อผิดพลาดแบบ exception
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-    // echo "เชื่อมต่อฐานข้อมูลสำเร็จ!";
+    echo "เชื่อมต่อฐานข้อมูลสำเร็จ!";
 } catch (PDOException $e) {
     echo "เชื่อมต่อฐานข้อมูลล้มเหลว: " . $e->getMessage();
 }
