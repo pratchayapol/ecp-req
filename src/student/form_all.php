00ec022f49e51015432b790bc283f208df88cf23
pratchayapol <pratchayapol2543@gmail.com>
2025-04-07 17:29:35 +0700

    a

--- src/student/form_all.php
+++ src/student/form_all.php
@@ -110,7 +110,7 @@ $course_level = $_SESSION['course_level'] ?? '';
 
 
                     <!-- Table -->
-                    <div class="overflow-x-auto w-full m-6 border border-gray-300 rounded-lg shadow-md">
+                    <div class="overflow-x-auto w-full ms-6 me-6 border border-gray-300 rounded-lg shadow-md">
                         <table class="min-w-full table-auto border-collapse">
                             <thead class="bg-orange-500 text-white text-left">
                                 <tr>
