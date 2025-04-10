74e2cf61a66adefec7a29d8cd0f1e7b74774aa05
pratchayapol <pratchayapol2543@gmail.com>
2025-04-10 14:49:09 +0700

    ss

--- src/index.php
+++ src/index.php
@@ -43,7 +43,7 @@ session_start();
                 มหาวิทยาลัยเทคโนโลยีราชมงคลอีสาน วิทยาเขตขอนแก่น
             </p>
             <!-- Button -->
-            <a href="google_auth.php"
+            <a href="google_auth"
                 class="mt-6 px-6 py-3 bg-blue-600 text-white font-semibold rounded-lg shadow-md 
           hover:bg-blue-700 hover:shadow-lg transition-all flex items-center gap-2">
                 <img src="https://upload.wikimedia.org/wikipedia/commons/0/09/IOS_Google_icon.png"
