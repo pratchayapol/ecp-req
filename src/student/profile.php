bf4394e8b89393c85e765a524c8ee860a0cebce6
pratchayapol <pratchayapol2543@gmail.com>
2025-05-03 23:47:18 +0700

    aa

--- src/student/profile.php
+++ src/student/profile.php
@@ -70,7 +70,7 @@ if (isset($_SESSION['user'])) {
     // เรียกใช้ฟังก์ชัน
     echo $academicYear = getAcademicYear(); // คำนวณปีการศึกษา
     // echo $academicLevel = getAcademicLevel($course_level, $academicYear);
-    echo $academicLevel = getAcademicLevel("ECP/R (64)", $academicYear);
+    echo $academicLevel = getAcademicLevel("ECP/R (63)", $academicYear);
 
 } else {
     header('location: ../session_timeout');
