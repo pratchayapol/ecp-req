d2ca87dfc026974dd9b75c57931aff0ae9fcb238
pratchayapol <pratchayapol2543@gmail.com>
2025-05-07 15:58:37 +0700

    aa

--- src/student/form_re06.php
+++ src/student/form_re06.php
@@ -3,8 +3,8 @@ session_start();
 include '../connect/dbcon.php';
 
 ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
-
 
 // echo '<pre>';
 // print_r($_SESSION);
