2b39b81cf689fadb740a1b1af2f37095aea7ca45
pratchayapol <pratchayapol2543@gmail.com>
2025-05-07 16:00:58 +0700

    aa

--- src/student/form_re06.php
+++ src/student/form_re06.php
@@ -447,7 +447,8 @@ if (isset($_GET['course_id'])) {
             $academicYear = $_POST['academicYear'] ?? '';
             $courseId = $_POST['course_id'] ?? '';
             $group = $_POST['academicGroup'] ?? '';
-            $teacher_email = $_POST['teacher_email'] ?? '';
+            echo $teacher_email = $_POST['teacher_email'] ?? '';
+
             $reason = $_POST['reason'] === 'other' ? ($_POST['other_reason'] ?? '') : $_POST['reason'];
             $registrations = $_POST['registrations'] ?? '';
             $regStatus = $_POST['reg_status'] ?? '';
