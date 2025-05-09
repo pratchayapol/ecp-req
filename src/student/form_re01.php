30fef56104ade861dd2afae09b6b032018eecd0f
pratchayapol <pratchayapol2543@gmail.com>
2025-05-09 19:40:03 +0700

    ปป

--- src/student/form_re01.php
+++ src/student/form_re01.php
@@ -356,11 +356,13 @@ $profile = $stmt->fetch(PDO::FETCH_ASSOC);
 
 
             
-// เรียกใช้ PHPMailer
-require '../vendor/autoload.php';
+require __DIR__ . '/../PHPMailer/src/Exception.php';
+require __DIR__ . '/../PHPMailer/src/PHPMailer.php';
+require __DIR__ . '/../PHPMailer/src/SMTP.php';
 
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
+
 $mail = new PHPMailer(true);
 
 try {
