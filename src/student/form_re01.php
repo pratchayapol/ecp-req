0e4fc2fe3aee78b70bb788c35c5e5a6202eaf315
pratchayapol <pratchayapol2543@gmail.com>
2025-05-12 20:52:40 +0700

    ปป

--- src/student/form_re01.php
+++ src/student/form_re01.php
@@ -372,10 +372,7 @@ $profile = $stmt->fetch(PDO::FETCH_ASSOC);
 
                 $mail->isHTML(true);
 
-                $mail->Body = '
-                 <style>
-        @import url("https://fonts.googleapis.com/css2?family=Noto+Sans+Thai&display=swap");
-    </style>
+                $mail->Body = 'แป
                 <div style="font-family: Tahoma, sans-serif; background-color:rgb(46, 46, 46); padding: 20px; border-radius: 10px; color: #f0f0f0; font-size: 18px;">
                     <h2 style="color: #ffa500; font-size: 24px;">📄 ยี่นคำร้องทั่วไป (RE.01)</h2>
                     <p style="margin-top: 10px;">เรียน <strong>' . htmlspecialchars($to) . '</strong>,</p>
