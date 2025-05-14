c94d2c177efe5597879498d18ae330ea111637d0
pratchayapol <pratchayapol2543@gmail.com>
2025-05-14 12:47:19 +0700

    ccss

--- src/student/form_re01.php
+++ src/student/form_re01.php
@@ -350,7 +350,7 @@ $profile = $stmt->fetch(PDO::FETCH_ASSOC);
             $token = generateToken(); //สร้างปุ่มและแนบ token คือ https://ecpreq.pcnone.com/sendmail_re1-1?token=
 
 
-            require_once __DIR__ . '/../vendor/autoload.php';
+            require_once __DIR__ . '/vendor/autoload.php';
 
 
             $mail = new PHPMailer(true);
