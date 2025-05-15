5d0cf305dbbd66503a1aa66657ffdd15f82b0e75
pratchayapol <pratchayapol2543@gmail.com>
2025-05-15 10:20:53 +0700

    cx

--- src/re06_1.php
+++ src/re06_1.php
@@ -238,12 +238,12 @@ include 'connect/dbcon.php';
                             line21.className = 'flex-auto h-0.5 mx-1 bg-gray-300';
                         }
 
-                        if (status === 0) {
+                        if (status === "0") {
                             // Case: ไม่พิจารณา
                             step1.className = 'w-8 h-8 rounded-full border-2 border-red-500 bg-red-500 text-white flex items-center justify-center';
                             step2.className = 'w-8 h-8 rounded-full border-2 border-red-500 bg-red-500 text-white flex items-center justify-center';
                             line21.className = 'flex-auto h-0.5 mx-1 bg-red-500';
-                        } else if (status === 1) {
+                        } else if (status === "1") {
                             // Case: อาจารย์พิจารณาแล้ว
                             step1.className = 'w-8 h-8 rounded-full border-2 border-green-500 bg-green-500 text-white flex items-center justify-center';
                             step2.className = 'w-8 h-8 rounded-full border-2 border-green-500 bg-green-500 text-white flex items-center justify-center';
