15168a482fab3342e032f30eaeb40c311d51901a
Artip <erisakifound@gmail.com>
2025-05-16 17:50:16 +0700

    dddd

--- src/PDF/report_pdf_re06.php
+++ src/PDF/report_pdf_re06.php
@@ -77,8 +77,8 @@ $pdf->Image('RE.06bg.jpg', 0, 0, 210, 297);
 $pdf->SetXY(190, 0);
 
 // //	ภาคเรียน
-$pdf->SetY(5);
-$pdf->SetX(100);
+$pdf->SetY(20);
+$pdf->SetX(120);
 $pdf->SetFont('sara', '', 14);
 $pdf->Cell(40, 2, iconv('utf-8', 'cp874', $term), 0, 1, 'L');
 
