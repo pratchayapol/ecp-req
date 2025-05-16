a124144ede79e6fea4befd1d42e50d54147f4c73
pratchayapol <pratchayapol2543@gmail.com>
2025-05-16 13:16:22 +0700

    xx

--- src/teacher/form_all.php
+++ src/teacher/form_all.php
@@ -698,7 +698,7 @@ function getNameByEmail($pdo, $email)
                                         <?php
                                         // การดึงข้อมูลจากฐานข้อมูล
                                         try {
-                                            $stmt3 = $pdo->prepare("SELECT 'RE07' as form_type, form_id as form_id, term, year, f.course_id, `group`, status, reason, gpa, git_unit, reg_status, expected_graduation, comment_teacher, comment_head_dep, token, email,
+                                            $stmt3 = $pdo->prepare("SELECT 'RE07' as form_type, form_id as form_id, term, year, f.course_id, `group`, status, reason, gpa, git_unit, reg_status, expected_graduation, comment_teacher, comment_head_dep, token,
 f.teacher_email, f.head_department,
 c.course_nameTH, c.credits
 FROM form_re07 AS f
