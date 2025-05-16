3332069c3c36e3c04080a5d2b5a021e4a70b51ea
Artip <erisakifound@gmail.com>
2025-05-16 17:33:55 +0700

    dddd

--- src/PDF/report_pdf_re06.php
+++ src/PDF/report_pdf_re06.php
@@ -80,7 +80,7 @@ $pdf->SetXY(190, 0);
 $pdf->SetY(5);
 $pdf->SetX(10);
 $pdf->SetFont('sara', '', 14);
-$pdf->Cell(40, 2, iconv('utf-8', 'cp874', $term), 0, 1, 'L');
+$pdf->Cell(40, 2, iconv('utf-8', 'cp874', $term), 0, 1, 'R');
 
 // //ปีการศึกษา	
 $pdf->SetY(49);
