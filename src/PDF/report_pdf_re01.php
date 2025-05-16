da8dbd133f10fbc4096a399d9e12b56e6f5fff9f
Artip <erisakifound@gmail.com>
2025-05-16 13:56:54 +0700

    ddd

--- src/PDF/report_pdf_re01.php
+++ src/PDF/report_pdf_re01.php
@@ -145,8 +145,8 @@ $pdf->Cell(42, 2, iconv('utf-8', 'cp874', $comment_head_dep), 0, 1, 'L');
 $created_at_thai = formatDateThai($created_at, ['  ', '   ', '    ', ' ']);
 // เว้นวรรคหลัง: วัน 2 ช่อง, เดือน 3 ช่อง, ปี 4 ช่อง, "เวลา" 1 ช่อง
 
-$pdf->SetY(40);
-$pdf->SetX(100);
+$pdf->SetY(35);
+$pdf->SetX(110);
 $pdf->SetFont('sara', '', 11.5);
 $pdf->Cell(42, 2, iconv('utf-8', 'cp874', $created_at_thai), 0, 1, 'R');
 
