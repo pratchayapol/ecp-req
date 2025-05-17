c2e0aab11f3e29cb82bef0f48615be3bd8fee0d4
Artip <erisakifound@gmail.com>
2025-05-17 17:32:57 +0700

    ddd

--- src/PDF/report_pdf_re01.php
+++ src/PDF/report_pdf_re01.php
@@ -161,10 +161,10 @@ $pdf->SetFont('sara', '', 11.5);
 $pdf->Cell(42, 2, iconv('utf-8', 'cp874', $created_at_thai), 0, 1, 'L');
 
 
-// //ชื่อ
-$pdf->SetY(40.5);
-$pdf->SetX(23);
-$pdf->SetFont('sara', '', 14);
-$pdf->Cell(40, 2, iconv('utf-8', 'cp874', $name), 0, 1, 'L');
+// // //ชื่อ
+// $pdf->SetY(40.5);
+// $pdf->SetX(23);
+// $pdf->SetFont('sara', '', 14);
+// $pdf->Cell(40, 2, iconv('utf-8', 'cp874', $name), 0, 1, 'L');
 
 $pdf->Output();
