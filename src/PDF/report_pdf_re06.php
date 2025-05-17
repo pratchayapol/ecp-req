62e6f9dafe9aa03b5e3bdd877232540234e099cc
Artip <erisakifound@gmail.com>
2025-05-17 22:46:18 +0700

    ddd

--- src/PDF/report_pdf_re06.php
+++ src/PDF/report_pdf_re06.php
@@ -188,20 +188,20 @@ $pdf->SetFont('sara', '', 14);
 $pdf->Cell(42, 2, iconv('utf-8', 'cp874', $id), 0, 1, 'L');
 
 // //คณะ
-$pdf->SetY(70);
+$pdf->SetY(75);
 $pdf->SetX(30.5);
 $pdf->SetFont('sara', '', 14);
 $pdf->Cell(42, 2, iconv('utf-8', 'cp874', $faculty), 0, 1, 'L');
 
 // //สาขาวิชา
-$pdf->SetY(120);
+$pdf->SetY(100);
 $pdf->SetX(100);
 $pdf->SetFont('sara', '', 14);
 $pdf->Cell(42, 2, iconv('utf-8', 'cp874', $field), 0, 1, 'L');
 
 // //ชั้นปี
-$pdf->SetY(130);
-$pdf->SetX(68.5);
+$pdf->SetY(75);
+$pdf->SetX(190);
 $pdf->SetFont('sara', '', 14);
 $pdf->Cell(42, 2, iconv('utf-8', 'cp874', $course_level), 0, 1, 'L');
 
