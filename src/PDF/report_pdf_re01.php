bd8c890da81c1a2b6feabb3c1c199e5b4dfa86ce
Artip <erisakifound@gmail.com>
2025-06-15 21:01:18 +0700

    ddd

--- src/PDF/report_pdf_re01.php
+++ src/PDF/report_pdf_re01.php
@@ -131,14 +131,14 @@ $pdf->Cell(168, 2, iconv('utf-8', 'cp874', $request_text), 0, 1, 'L');
 
 // //ความคิดเห็นอาจารย์ที่ปรึกษา
 $pdf->SetY(138);
-$pdf->SetX(15);
+$pdf->SetX(16);
 $pdf->SetFont('sara', '', 14);
 $pdf->Cell(42, 2, iconv('utf-8', 'cp874', $comment_teacher), 0, 1, 'L');
 
 
 // //ความคิดเห็นหัวหน้าสาขา
 $pdf->SetY(173);
-$pdf->SetX(15);
+$pdf->SetX(16);
 $pdf->SetFont('sara', '', 14);
 $pdf->Cell(42, 2, iconv('utf-8', 'cp874', $comment_head_dep), 0, 1, 'L');
 
