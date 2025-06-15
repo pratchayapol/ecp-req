d14eaba533aad2b3ebb8251009cb5a1e5f917bdf
Artip <erisakifound@gmail.com>
2025-06-15 22:27:05 +0700

    ddd

--- src/PDF/report_pdf_re07.php
+++ src/PDF/report_pdf_re07.php
@@ -95,12 +95,12 @@ $pdf->SetXY(190, 0);
 $pdf->SetFont('sara', '', 14);
 
 // // ภาคเรียน1
-$pdf->SetY(20);
+$pdf->SetY(15);
 $pdf->SetX(140);
 $pdf->Cell(40, 8, iconv('utf-8', 'cp874', $term), 0, 1, 'L');
 
 // // ปีการศึกษา1
-$pdf->SetY(20);
+$pdf->SetY(15);
 $pdf->SetX(172);
 $pdf->Cell(40, 8, iconv('utf-8', 'cp874', $year), 0, 1, 'L');
 
@@ -150,32 +150,26 @@ $pdf->Cell(100, 8, iconv('utf-8', 'cp874', $email), 0, 1, 'L');
 $created_at_thai = formatDateThai($created_at, ['              ','            ','    ', ' ']);
 // เว้นวรรคหลัง: วัน 2 ช่อง, เดือน 3 ช่อง, ปี 4 ช่อง, "เวลา" 1 ช่อง
 
-$pdf->SetY(36);
+$pdf->SetY(31);
 $pdf->SetX(117);
 $pdf->SetFont('sara', '', 14);
 $pdf->Cell(42, 2, iconv('utf-8', 'cp874', $created_at_thai), 0, 1, 'L');
 
 // //ชื่อ สกุล
-$pdf->SetY(53.5);
+$pdf->SetY(49);
 $pdf->SetX(68.5);
 $pdf->SetFont('sara', '', 14);
 $pdf->Cell(42, 2, iconv('utf-8', 'cp874', $name), 0, 1, 'L');
 
 // //เลขนศ
-$pdf->SetY(53.5);
+$pdf->SetY(49);
 $pdf->SetX(160);
 $pdf->SetFont('sara', '', 14);
 $pdf->Cell(42, 2, iconv('utf-8', 'cp874', $id), 0, 1, 'L');
 
-// //คณะ
-$pdf->SetY(71.5);
-$pdf->SetX(30.5);
-$pdf->SetFont('sara', '', 14);
-$pdf->Cell(42, 2, iconv('utf-8', 'cp874', $faculty), 0, 1, 'L');
-
 // //สาขาวิชา
-$pdf->SetY(71.5);
-$pdf->SetX(100);
+$pdf->SetY(73);
+$pdf->SetX(40);
 $pdf->SetFont('sara', '', 14);
 $pdf->Cell(42, 2, iconv('utf-8', 'cp874', $field), 0, 1, 'L');
 
